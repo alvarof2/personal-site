96f3e64866c413f600c56e3c5577d21b27f7c22a
alvarof2 <fernandez.f.alvaro@gmail.com>
2022-04-13 09:47:29 +0200

    Repetition

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -41,7 +41,7 @@ const Resume = () => (
 
         </div>
       </header>
-      <p> My resume is also available in <a href="https://drive.google.com/file/d/1ari72Hmw6ajKEuFt8PRNN4EZDINqccUE/view?usp=sharing">pdf format</a>) with source code available <a href="https://github.com/alvarof2/latex-cv">here</a>.
+      <p> My resume is also available in <a href="https://drive.google.com/file/d/1ari72Hmw6ajKEuFt8PRNN4EZDINqccUE/view?usp=sharing">pdf format</a> with source code <a href="https://github.com/alvarof2/latex-cv">here</a>.
       </p>
       <Education data={degrees} />
       <Experience data={positions} />
